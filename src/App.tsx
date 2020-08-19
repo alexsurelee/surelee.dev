e5ab37869aa80d7d44351956ecffb13149752750
Alex <alexgrlee@gmail.com>
2020-08-19 20:03:26 +1200

    Initial shitty layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+        <p className="header">Alexander Lee</p>
+        <p className="subtext">@alexsurelee</p>
+        <ButtonGroup variant="text">
+          <Button onClick={() => window.open("https://github.com/alexsurelee")}>
+            GitHub
+          </Button>
+          <Button onClick={() => window.open("mailto:alex@surelee.dev")}>
+            Email
+          </Button>
+          <Button>Portfolio</Button>
+        </ButtonGroup>
       </header>
     </div>
   );
