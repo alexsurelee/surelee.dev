791546d448bfd7d3fd1aed7a7710021f786969b6
Alex <alexgrlee@gmail.com>
2020-08-22 18:47:05 +1200

    Add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
+import AppBar from "@material-ui/core/AppBar";
+import MenuIcon from "@material-ui/icons/Menu";
 import logo from "./assets/surelee256.png";
 import "./App.css";
+import { Toolbar, IconButton, Typography } from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
+      <AppBar position="static" color="transparent">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography className="navBar" variant="h6">
+            Alexander Lee
+          </Typography>
+        </Toolbar>
+      </AppBar>
       <header className="App-header">
         <img src={logo} alt="surelee logo" />
         <p className="subtext">@alexsurelee</p>
