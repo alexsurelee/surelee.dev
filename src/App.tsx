beff34fc956e3cc7d42a632779bd0a9ef1340c6c
Alex <alexgrlee@gmail.com>
2020-08-22 16:27:13 +1200

    Add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
+import logo from "./assets/surelee256.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} alt="surelee logo" />
         <p className="header">Alexander Lee</p>
         <p className="subtext">@alexsurelee</p>
         <ButtonGroup variant="text">
