c01b42cc6f8e6c73c097763642949a6fb498d1a0
Alex <alexgrlee@gmail.com>
2020-11-21 14:48:33 +1300

    Update to typescript 4 and remove material packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,31 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
-import ButtonGroup from "@material-ui/core/ButtonGroup";
-import AppBar from "@material-ui/core/AppBar";
-import MenuIcon from "@material-ui/icons/Menu";
-import logo from "./assets/surelee256.png";
 import "./App.css";
 import "mailgo/dist/mailgo.min.js";
-import { Toolbar, IconButton, Typography } from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
-      <AppBar position="static" color="transparent">
-        <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography className="navBar" variant="h6">
-            Alexander Lee
-          </Typography>
-        </Toolbar>
-      </AppBar>
       <header className="App-header">
-        <img src={logo} alt="surelee logo" />
+        <p>ALEXANDER LEE</p>
         <p className="subtext">@alexsurelee</p>
-        <ButtonGroup variant="text">
-          <Button onClick={() => window.open("https://github.com/alexsurelee")}>
+        <div className="button-group">
+          <button onClick={() => window.open("https://github.com/alexsurelee")}>
             GitHub
-          </Button>
-          <Button>
-            <a
-              href="#mailgo"
-              data-address="alex"
-              data-domain="surelee.dev"
-              className="dark"
-            >
-              Email
-            </a>
-          </Button>
-          <Button
+          </button>
+          <a
+            href="#mailgo"
+            data-address="alex"
+            data-domain="surelee.dev"
+            className="dark"
+          >
+            Email
+          </a>
+          <button
             onClick={() => window.open("https://linkedin.com/in/alexsurelee")}
           >
             LinkedIn
-          </Button>
-          <Button>Portfolio</Button>
-        </ButtonGroup>
+          </button>
+        </div>
       </header>
     </div>
   );
