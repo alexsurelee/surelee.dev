ec5951f097150f0eb1f9ab8c2298398094dbfe72
Alex <alexgrlee@gmail.com>
2020-11-21 15:15:01 +1300

    Make links open in new tabs

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,9 @@ function App() {
       <header className="App-header">
         <h1>ALEXANDER LEE</h1>
         <div className="button-group">
-          <a href="https://github.com/alexsurelee">GITHUB</a>
+          <a href="https://github.com/alexsurelee" target="_blank">
+            GITHUB
+          </a>
           <a
             href="#mailgo"
             data-address="alex"
@@ -19,7 +21,9 @@ function App() {
           >
             EMAIL
           </a>
-          <a href="https://linkedin.com/in/alexsurelee">LINKEDIN</a>
+          <a href="https://linkedin.com/in/alexsurelee" target="_blank">
+            LINKEDIN
+          </a>
         </div>
       </header>
     </div>
