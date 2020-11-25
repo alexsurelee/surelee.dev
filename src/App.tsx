16f52dfdc2949d3cead41f0a6699d5eda3701ba5
Alex <alexgrlee@gmail.com>
2020-11-25 20:26:06 +1300

    Add ✨sass✨

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,11 @@ function App() {
       <header className="App-header">
         <h1>ALEXANDER LEE</h1>
         <div className="button-group">
-          <a href="https://github.com/alexsurelee" target="_blank">
+          <a
+            href="https://github.com/alexsurelee"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             GITHUB
           </a>
           <a
@@ -21,10 +25,15 @@ function App() {
           >
             EMAIL
           </a>
-          <a href="https://linkedin.com/in/alexsurelee" target="_blank">
+          <a
+            href="https://linkedin.com/in/alexsurelee"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             LINKEDIN
           </a>
         </div>
+        <div className="button-group"></div>
       </header>
     </div>
   );
