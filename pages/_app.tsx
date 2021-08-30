c2d0d939d15c321888cc642383867d2f8b8596a1
Alex <alexgrlee@gmail.com>
2021-08-30 17:52:25 +1200

    Update template with some personal details

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import mailgo from "mailgo";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    mailgo();
+  });
+  return <Component {...pageProps} />;
 }
-export default MyApp
+export default MyApp;
