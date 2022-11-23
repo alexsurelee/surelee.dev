829e50a8db5127581d356ffd803a90d9f42d3e58
Alexander Lee <alex@surelee.dev>
2022-11-23 22:37:50 +1300

    Add typewriter styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import styles from "../styles/Home.module.scss";
+import styles from "../styles/home.module.scss";
 
 const Home: NextPage = () => {
   return (
@@ -10,40 +10,11 @@ const Home: NextPage = () => {
         <meta name="description" content="Web development services for " />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>Alexander Lee</h1>
-
-        {/* <p className={styles.description}>
-          Portfolio coming soon...
-        </p>
-
-        <div className={styles.grid}>
-          <a href="#" className={styles.card}>
-            <h2>Experience &rarr;</h2>
-            <p>
-              See why I&apos;m qualified to develop a solution for your needs.
-            </p>
-          </a>
-
-          <a href="https://github.com/alexsurelee" className={styles.card}>
-            <h2>GitHub &rarr;</h2>
-            <p>
-              Review some of my public projects and open source contributions.
-            </p>
-          </a>
-
-          <a href="#" className={styles.card}>
-            <h2>Portfolio &rarr;</h2>
-            <p>Play around with some of my recent projects!</p>
-          </a>
-
-          <a href="https://linkedin.com/in/alexsurelee" className={styles.card}>
-            <h2>LinkedIn &rarr;</h2>
-            <p>Check out my journey so far, and connect with me!</p>
-          </a>
-        </div> */}
-      </main>
+      <header className={styles.header}>
+        <h1 className={styles.name}>Alexander Lee</h1>
+        <h2 className={styles.jobTitle}>UX Engineer</h2>
+      </header>
+      <main className={styles.main}></main>
 
       <footer className={styles.footer}>
         <a
