87689bad93846569ea49930630675585135fe854
Alex <alexgrlee@gmail.com>
2023-01-07 18:57:56 +1300

    Chuck a card in there

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Card, CardTitle } from "components/Card";
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.scss";
@@ -7,42 +8,15 @@ const Home: NextPage = () => {
     <div className={styles.container}>
       <Head>
         <title>Alexander Lee</title>
-        <meta name="description" content="Web development services for " />
+        <meta name="description" content="Alexander Lee - Portfolio " />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>Alexander Lee</h1>
-
-        {/* <p className={styles.description}>
-          Portfolio coming soon...
-        </p>
-
-        <div className={styles.grid}>
-          <a href="#" className={styles.card}>
-            <h2>Experience &rarr;</h2>
-            <p>
-              See why I&apos;m qualified to develop a solution for your needs.
-            </p>
-          </a>
-
-          <a href="https://github.com/alexsurelee" className={styles.card}>
-            <h2>GitHub &rarr;</h2>
-            <p>
-              Review some of my public projects and open source contributions.
-            </p>
-          </a>
-
-          <a href="#" className={styles.card}>
-            <h2>Portfolio &rarr;</h2>
-            <p>Play around with some of my recent projects!</p>
-          </a>
-
-          <a href="https://linkedin.com/in/alexsurelee" className={styles.card}>
-            <h2>LinkedIn &rarr;</h2>
-            <p>Check out my journey so far, and connect with me!</p>
-          </a>
-        </div> */}
+        <Card>
+          <CardTitle>Yellow</CardTitle>
+        </Card>
       </main>
 
       <footer className={styles.footer}>
